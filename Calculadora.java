3c6d46e1d2cd78737f2547b087cf52885587ffc7
frede122 <fredemanduoliveira@hotmail.com>
2019-06-25 12:39:12 -0300

    Inclusão de Exception

--- Calculadora.java
+++ Calculadora.java
@@ -1,5 +1,4 @@
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
+import javax.swing.JOptionPane;
 
 import javax.swing.JButton;
 import javax.swing.JTextField;
@@ -9,9 +8,9 @@ import javax.swing.JFrame;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
+import java.lang.ArithmeticException;
 
-
-public class Calculadora implements ActionListener{
+public class Calculadora implements ActionListener {
 
     static JTextField text1 = new JTextField(10);
     static JTextField text2 = new JTextField(10);
@@ -20,7 +19,6 @@ public class Calculadora implements ActionListener{
     static JLabel label1 = new JLabel("Valor 1:");
     static JLabel label2 = new JLabel("Valor 2:");
     static JLabel label3 = new JLabel("Resultado:");
-
 
     static JButton btnProximo = new JButton("Proximo");
     static JButton btnClear = new JButton("Clear");
@@ -43,33 +41,20 @@ public class Calculadora implements ActionListener{
     static JButton btn9 = new JButton("9");
     static JButton btn0 = new JButton("0");
     static JTextField field;
-    static float a=1,b=1, result =0;
-	
+    static float a = 1, b = 1, result = 0;
 
     static Calculadora j = new Calculadora();
 
-
-    public static void main(String args[]){
+    public static void main(String args[]) {
 
         JFrame janela1 = new JFrame();
         janela1.setLayout(null);
-        janela1.setSize(295,350);
-
-
-
-
-
-        
-	
-	
-
+        janela1.setSize(295, 350);
 
         label1.setBounds(5, 10, 70, 25);
         label2.setBounds(5, 40, 70, 25);
         label3.setBounds(5, 70, 70, 25);
 
-        
-        
         text1.addFocusListener(new java.awt.event.FocusAdapter() {
             public void focusLost(java.awt.event.FocusEvent e) {
                 field = text1;
@@ -85,7 +70,7 @@ public class Calculadora implements ActionListener{
         text1.setBounds(90, 10, 185, 25);
         text2.setBounds(90, 40, 185, 25);
         text3.setBounds(90, 70, 185, 25);
-        //text3.disable();
+        // text3.disable();
         text3.setText("");
 
         btnProximo.setBounds(5, 110, 200, 25);
@@ -93,18 +78,16 @@ public class Calculadora implements ActionListener{
         btnMais.setBounds(225, 110, 50, 25);
         btnMais.addActionListener(j);
 
-
         btn1.setBounds(5, 145, 50, 30);
         btn1.setMnemonic(1);
         btn1.addActionListener(j);
-        btn2.setBounds(85, 145,  50, 30);
+        btn2.setBounds(85, 145, 50, 30);
         btn2.addActionListener(j);
-        btn3.setBounds(155, 145,  50, 30);
+        btn3.setBounds(155, 145, 50, 30);
         btn3.addActionListener(j);
         btnMenos.setBounds(225, 145, 50, 30);
         btnMenos.addActionListener(j);
 
-        
         btn4.setBounds(5, 185, 50, 30);
         btn4.addActionListener(j);
         btn5.setBounds(85, 185, 50, 30);
@@ -114,8 +97,7 @@ public class Calculadora implements ActionListener{
         btnDiv.setBounds(225, 185, 50, 30);
         btnDiv.addActionListener(j);
 
- 
-        btn7.setBounds(5, 225, 50, 30);    
+        btn7.setBounds(5, 225, 50, 30);
         btn7.addActionListener(j);
         btn8.setBounds(85, 225, 50, 30);
         btn8.addActionListener(j);
@@ -128,33 +110,22 @@ public class Calculadora implements ActionListener{
         btnClear.addActionListener(j);
         btn0.setBounds(85, 265, 50, 30);
         btn0.addActionListener(j);
-        btnExit.setBounds(145, 265, 60, 30);    
+        btnExit.setBounds(145, 265, 60, 30);
         btnExit.addActionListener(j);
         btnIgual.setBounds(225, 265, 50, 30);
         btnIgual.addActionListener(j);
-        
- 
-     
-        
 
-        
-
-        
-        
-       
-        
         janela1.add(label1);
         janela1.add(text1);
-        
+
         janela1.add(label2);
         janela1.add(text2);
-        
+
         janela1.add(label3);
         janela1.add(text3);
 
         janela1.add(btnProximo);
         janela1.add(btnMais);
-
 
         janela1.add(btn1);
         janela1.add(btn2);
@@ -176,87 +147,117 @@ public class Calculadora implements ActionListener{
         janela1.add(btnExit);
         janela1.add(btnIgual);
 
-
-
-
-
-
-        
         janela1.setVisible(true);
-        
 
     }
 
-    public void limpaCaixa(JTextField f1, JTextField f2){
-        f1.setText("");
-        f2.setText("");
-        f1.requestFocus();
+    public void limpaCaixa(JTextField f) {
+        f.setText("");
+        f.requestFocus();
     }
 
-    public void limpaCaixa(JTextField f1, JTextField f2, JTextField f3){
+    public void limpaCaixa(JTextField f1, JTextField f2) {
+        limpaCaixa(f2);
+        limpaCaixa(f1);
+
+    }
+
+    public void limpaCaixa(JTextField f1, JTextField f2, JTextField f3) {
         f3.setText("");
         limpaCaixa(f1, f2);
     }
 
-    public void actionPerformed(ActionEvent evt){
-            Object obj = evt.getSource();
-            
-            if(btn0.equals(obj) || btn1.equals(obj) || btn2.equals(obj) || btn3.equals(obj) || btn4.equals(obj) || btn5.equals(obj) || btn6.equals(obj) || btn7.equals(obj) || btn8.equals(obj) || btn9.equals(obj)){
-                JButton bt = (JButton)obj;
-                if(field.equals(text1)){
-                    text1.setText(text1.getText() + bt.getText());
-                }else if(field.equals(text2)){
-                    text2.setText(text2.getText() + bt.getText());
-                }
-		
-                
-                
-            }
+    public float gravarValor(JTextField f) {
+        float r = 0;
+        try {
+            r = Float.parseFloat(f.getText());
 
-            if(btnProximo.equals(obj)){
-                text2.requestFocus();
-            }
+        } catch (Exception ae) {
 
-            if(btnMais.equals(obj)){
-                a = Float.parseFloat(text1.getText());
-                b = Float.parseFloat(text2.getText());
-                result = a + b;
-                
-            }
-            
-            if(btnMenos.equals(obj)){
-                a = Float.parseFloat(text1.getText());
-                b = Float.parseFloat(text2.getText());
-                result = a - b;
+            JOptionPane.showMessageDialog(null,
+                    "Valor não numerico, insira novamente os dados \n erro: " + ae.getMessage());
+            limpaCaixa(f);
+        }
 
-            }
-            if(btnDiv.equals(obj)){
-                a = Float.parseFloat(text1.getText());
-                b = Float.parseFloat(text2.getText());
-                result = a / b;
-                System.out.println(result);
-            }
-
-            if(btnMulti.equals(obj)){
-                a = Float.parseFloat(text1.getText());
-                b = Float.parseFloat(text2.getText());
-                result = a * b;
-            }
-
-            if(btnIgual.equals(obj)){
-                text3.setText(String.valueOf(result));
-                limpaCaixa(text1, text2);
-            }
-
-            if(btnClear.equals(obj)){
-                limpaCaixa(text1, text2, text3);
-            }
-
-            if(btnExit.equals(obj)){
-                System.exit(0);
-            }
+        return r;
 
     }
 
+    public void actionPerformed(ActionEvent evt) {
+        Object obj = evt.getSource();
+
+        if (btn0.equals(obj) || btn1.equals(obj) || btn2.equals(obj) || btn3.equals(obj) || btn4.equals(obj)
+                || btn5.equals(obj) || btn6.equals(obj) || btn7.equals(obj) || btn8.equals(obj) || btn9.equals(obj)) {
+            JButton bt = (JButton) obj;
+            if (field.equals(text1)) {
+                text1.setText(text1.getText() + bt.getText());
+            } else if (field.equals(text2)) {
+                text2.setText(text2.getText() + bt.getText());
+            }
+
+        }
+
+        if (btnProximo.equals(obj)) {
+            text2.requestFocus();
+        }
+
+        if (btnMais.equals(obj)) {
+            a = gravarValor(text1);
+            b = gravarValor(text2);
+            result = Func.soma(a, b);
+
+        }
+
+        if (btnMenos.equals(obj)) {
+            a = gravarValor(text1);
+            b = gravarValor(text2);
+            try {
+                result = Func.subtrair(a, b);
+            } catch (NegaException ne) {
+                ne.valorNegativo();
+                limpaCaixa(text2);
+            }
+
+        }
+
+        if (btnMulti.equals(obj)) {
+            a = gravarValor(text1);
+            b = gravarValor(text2);
+            result = a * b;
+
+        }
+
+        if (btnDiv.equals(obj)) {
+            a = gravarValor(text1);
+            b = gravarValor(text2);
+            try {
+                result = Func.dividir(a, b);
+            } catch (ArithmeticException ae) {
+                JOptionPane.showMessageDialog(null,
+                        "Não existe divisão por zero, insira novamente o valor erro: " + ae.getMessage());
+                if (a == 0) {
+                    limpaCaixa(text1);
+                } else if (b == 0) {
+                    limpaCaixa(text2);
+                }
+            }
+
+        }
+
+        if (btnIgual.equals(obj)) {
+            text3.setText(String.valueOf(result));
+            limpaCaixa(text1, text2);
+        }
+
+        if (btnClear.equals(obj)) {
+            limpaCaixa(text1, text2, text3);
+
+        }
+
+        if (btnExit.equals(obj)) {
+            System.exit(0);
+        }
+
+    }
 
 }
